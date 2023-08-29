5da3257e9e27124242bda8bb346c243f7b229ece
ElsidRickPanolino <202180195@psu.palawan.edu.ph>
2023-08-29 17:49:39 +0800

    Fix home and menu design in other device

--- script.js
+++ script.js
@@ -1,16 +1,3 @@
-function showOnClick(id){
-    var element = document.getElementById(id)
-
-    if(element.style.display=="none"){
-        element.style.display="flex"
-    }
-    else{
-        element.style.display="none"
-    }
-}
-
-function alertOrder(id){
-    var element = document.getElementById(id)
-    alert("ORDER PLACED")
-    element.style.display="initial"
+function alertOrder(name){
+    alert(`You ordered ${name}`)
 }
